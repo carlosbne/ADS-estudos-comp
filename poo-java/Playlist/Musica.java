b578ad73b1c0fe9f833da6908b5f55746f1778e9
carlosbne <carlosbneto72@gmail.com>
2024-08-16 10:26:00 -0300

    atividade 16/08

--- poo-java/Playlist/Musica.java
+++ poo-java/Playlist/Musica.java
@@ -1,3 +1,54 @@
+import java.util.Date;
+
 class Musica{
-    
+  private String titulo;
+  private String artista;
+  private String album;
+  private Timespan duracao;
+  private Date data;
+
+  public Musica(String titulo, String artista, String album, int duracao, Date data){
+    this.titulo = titulo;
+    this.artista = artista;
+    this.album = album;
+    this.duracao = duracao;
+    this.data = data;
+  }
+
+  public String getTitulo(){
+    return this.titulo;
+  }
+
+  public String getArtista(){
+    return this.artista;
+  }
+
+  public String getAlbum(){
+    return this.album;
+  }
+
+  public int getDuracao(){
+    return this.duracao;
+  }
+
+  public Date getData(){
+    return this.data;
+  }
+
+  public void setTitulo(String titulo){
+    this.titulo = titulo;
+  }
+
+  public void setArtista(String artista){
+    this.artista = artista;
+  }
+
+  public void setAlbum(String album){
+    this.album = album;
+  }
+
+  @Override
+  public String toString(){
+    return "Titulo: " + this.titulo + ", Artista: " + this.artista + ", Album: " + this.album + ", Duracao: " + this.duracao + ", Data: " + this.data;
+  }
 }
--- poo-java/Playlist/Playlist.java
+++ poo-java/Playlist/Playlist.java
@@ -1,4 +1,38 @@
-class Playlist{
+import java.util.ArrayList;
+import java.util.List;
 
-    
+public @interface Timespan;
+
+public class Playlist {
+  private String nome;
+  private String descricao;
+  private List<Musica> musicas;
+
+  public Playlist(String nome, String descricao) {
+    this.nome = nome;
+    this.descricao = descricao;
+    this.musicas = new ArrayList<Musica>();
+  }
+
+  public void inserir(Musica musica) {
+    musicas.add(musica);
+  }
+
+  public void listar(Musica musica) {
+    for (Musica m : musicas) {
+      System.out.println(m + " ,");
+    }
+  }
+  public Timespan tempoTotal(){
+    Timespan tempoTotal = new Timespan();
+    for (Musica m : musicas) {
+      tempoTotal.add(m.getDuracao());
+    }
+    return tempoTotal;
+  }
+
+  @Override
+  public String toString(){
+    return "Nome: " + this.nome + ", Descricao: " + this.descricao + ", Musicas: " + this.musicas;
+  }
 }
